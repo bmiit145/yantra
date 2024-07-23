9637a1c3eefba74f9e97e10284fd0427ee24513e
vkpatel18 <125430817+vkpatel18@users.noreply.github.com>
2024-07-23 17:14:30 +0530

    404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,54 @@
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable" data-theme="default" data-theme-colors="default"><head>
+
+    <meta charset="utf-8">
+    <title>404 Error Cover | Velzon - Admin &amp; Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin &amp; Dashboard Template" name="description">
+    <meta content="Themesbrand" name="author">
+    <!-- App favicon -->
+   
+
+    <!-- Layout config Js -->
+ 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Icons Css -->
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css">
+    <!-- App Css-->
+    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css">
+    <!-- custom Css-->
+    <link href="assets/css/custom.min.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+    <!-- auth-page wrapper -->
+    <div class="auth-page-wrapper py-5 d-flex justify-content-center align-items-center min-vh-100">
+
+        <!-- auth-page content -->
+        <div class="auth-page-content overflow-hidden p-0">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-7 col-lg-8">
+                        <div class="text-center">
+                            <img src="images/error/error400-cover.png" alt="error img" class="img-fluid">
+                            <div class="mt-3">
+                                <h3 class="text-uppercase">Sorry, Page not Found 😭</h3>
+                                <p class="text-muted mb-4">The page you are looking for not available!</p>
+                                <a href="{{ route('login') }}" class="btn btn-success"><i class="mdi mdi-home me-1"></i>Back to home</a>
+                            </div>
+                        </div>
+                    </div><!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end auth-page content -->
+    </div>
+    <!-- end auth-page-wrapper -->
+
+
+
+</body></html>
