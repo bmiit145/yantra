9a648b475a2001ab42b12d8c858f1fd0bb56c597
bmiit145 <21bmiit145@gmail.com>
2024-07-26 12:51:37 +0530

    inviteMail namespace

--- app/Mail/InviteMail.php
+++ app/Mail/InviteMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class inviteMail extends Mailable
+class InviteMail extends Mailable
 {
     use Queueable, SerializesModels;
 
